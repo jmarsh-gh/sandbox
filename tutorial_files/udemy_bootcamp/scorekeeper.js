b05f7877b066ec65ac44dbf9308719fdbc5358e5
jmarsh <j.marshmoto@gmail.com>
2016-11-30 09:05:34 +0000

    save

--- tutorial_files/udemy_bootcamp/scorekeeper.js
+++ tutorial_files/udemy_bootcamp/scorekeeper.js
@@ -3,6 +3,8 @@ var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var resetButton = document.getElementById("reset");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -30,7 +32,11 @@ p2Button.addEventListener("click", function(){
 	}
 });
 
-resetButton.addEventListener("click", function(){	
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
 	p1Score = 0;
 	p2Score = 0;
 	p1Display.textContent = 0;
@@ -38,4 +44,11 @@ resetButton.addEventListener("click", function(){
 	p1Display.classList.remove("winner");
 	p2Display.classList.remove("winner");
 	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
 });
+
