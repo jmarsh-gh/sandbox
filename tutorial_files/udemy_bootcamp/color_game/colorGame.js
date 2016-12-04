f4df1ea39d03d4d03bbf65eb110b9ea2eb62b216
jmarsh <j.marshmoto@gmail.com>
2016-12-04 16:57:05 +0000

    basic outline

--- tutorial_files/udemy_bootcamp/color_game/colorGame.js
+++ tutorial_files/udemy_bootcamp/color_game/colorGame.js
@@ -1,1 +1,30 @@
-alert("connected!");
+//alert("connected!");
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)",
+];
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+
+var colorDisplay = document.querySelector("#colorDisplay");
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	//add initital colors to each square
+	squares[i].style.background = colors[i];
+	//add click listeners to each square
+	squares[i].addEventListener("click", function(){
+		//get color of clicked square
+		var clickedColor = this.style.background;
+		//compare clicked color to pickedColor
+		if(clickedColor === pickedColor){
+			alert("correct");
+		} else {
+			alert("wrong");
+		}
+	})
+}
